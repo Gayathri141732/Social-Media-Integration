7b1a25915e2caa3491add10ccdbe8bc55b095c31
gayathri <gayathrisgsret@gmail.com>
2021-03-18 14:37:55 +0530

    Social-Media-Integration

---
+++ app/src/main/java/com/example/gripproject/FacebookProfileActivity.java
@@ -0,0 +1,134 @@
+package com.example.gripproject;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.facebook.AccessToken;
+import com.facebook.AccessTokenTracker;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.HttpMethod;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+import com.facebook.login.widget.ProfilePictureView;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.InputStream;
+import java.util.Arrays;
+
+public class FacebookProfileActivity extends AppCompatActivity {
+
+    private CallbackManager callbackManager;
+    private LoginManager loginManager;
+    private TextView nameView, emailView;
+    private ImageView profilePhoto;
+    private ProfilePictureView profilePictureView;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_facebookprofile);
+
+        nameView = findViewById(R.id.name);
+        emailView = findViewById(R.id.email);
+        LoginButton logInButton = findViewById(R.id.fb_button);
+        profilePictureView = (ProfilePictureView)findViewById(R.id.fb_profile_image);
+
+        callbackManager = CallbackManager.Factory.create();
+        logInButton.setReadPermissions(Arrays.asList("email", "public_profile"));
+
+        boolean loggedOut = AccessToken.getCurrentAccessToken() == null;
+        if(!loggedOut)
+            facebookLogin(AccessToken.getCurrentAccessToken());
+
+        logInButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+
+            }
+
+            @Override
+            public void onCancel() {
+
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    AccessTokenTracker tracker = new AccessTokenTracker() {
+        @Override
+        protected void onCurrentAccessTokenChanged(AccessToken oldAccessToken, AccessToken currentAccessToken) {
+            if (currentAccessToken == null) {
+                nameView.setText("");
+                emailView.setText("");
+                profilePhoto.setImageResource(0);
+                Toast.makeText(FacebookProfileActivity.this, "Signed out successfully", Toast.LENGTH_LONG).show();
+            }
+            else {
+                facebookLogin(currentAccessToken);
+            }
+        }
+    };
+
+    private void facebookLogin(AccessToken newAccessToken) {
+        Log.i("MainActivity", "----Inside facebookLogin method----");
+
+        GraphRequest graphRequest = GraphRequest.newMeRequest(
+                newAccessToken, new GraphRequest.GraphJSONObjectCallback() {
+                    @Override
+                    public void onCompleted(JSONObject object, GraphResponse response) {
+                        if(object != null) {
+                            try {
+                                String first_name = object.getString("first_name");
+                                String last_name = object.getString("last_name");
+                                String email = object.getString("email");
+                                String id = object.getString("id");
+
+                                nameView.setText(first_name + " " + last_name);
+                                Log.i("ProfileDetails", first_name + " " + last_name);
+
+                                emailView.setText(email);
+                                Log.i("ProfileDetails", email);
+
+                                profilePictureView.setProfileId(id);
+                            }
+                            catch (JSONException | NullPointerException e){
+                                e.printStackTrace();
+                            }
+                        }
+                    }
+                });
+        Bundle parameters = new Bundle();
+        parameters.putString("fields", "first_name, last_name, email, id");
+        graphRequest.setParameters(parameters);
+        graphRequest.executeAsync();
+    }
+}
---
+++ app/src/main/java/com/example/gripproject/MainActivity.java
@@ -0,0 +1,125 @@
+package com.example.gripproject;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
+import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.HttpMethod;
+import com.facebook.appevents.AppEventsLogger;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInApi;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.tasks.Task;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
+
+
+public class MainActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener {
+
+    private static final int RC_SIGN_IN = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+
+        GoogleProfileActivity.mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        SignInButton signInButton = findViewById(R.id.sign_in_button);
+        signInButton.setSize(SignInButton.SIZE_STANDARD);
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent signInIntent = GoogleProfileActivity.mGoogleSignInClient.getSignInIntent();
+                startActivityForResult(signInIntent, RC_SIGN_IN);
+            }
+        });
+
+        findViewById(R.id.fb_login_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent fbIntent = new Intent(getApplicationContext(), FacebookProfileActivity.class);
+                startActivity(fbIntent);
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+
+        if (account != null) {
+            GoogleProfileActivity.mAccount = account;
+            startActivity(new Intent(this, GoogleProfileActivity.class));
+            finish();
+        }
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN) {
+            Task<GoogleSignInAccount> result = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(result);
+        }
+    }
+
+    private void handleSignInResult(Task<GoogleSignInAccount> result) {
+        try {
+            GoogleSignInAccount account = result.getResult(ApiException.class);
+
+            if (account != null) {
+                GoogleProfileActivity.mAccount = account;
+                startActivity(new Intent(this, GoogleProfileActivity.class));
+                finish();
+            }
+        } catch (ApiException e) {
+            Log.w("MainActivity", "signInResult:failed code=" + e.getStatusCode());
+        }
+    }
+}
